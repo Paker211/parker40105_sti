a086aa89e2f383a7cfeb2447b13dc4908818c87b
parker <parker40105@gmail.com>
2021-04-17 10:41:38 +0800

    Start

---
+++ dic1/hello/hello.js
@@ -0,0 +1,9 @@
+let temperature;
+temperature = 30;
+console.log(temperature);
+
+temperature = 37;
+console.log(temperature);
+
+const days = 7;
+console.log(days);
---
+++ dic1/test1/test1.js
@@ -0,0 +1,6 @@
+let user = {
+    name: "John",
+    age: 18,
+};
+console.log(user);
+console.log(user.name);
